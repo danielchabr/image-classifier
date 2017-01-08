820de3dafd9944fce30f7c010e59e5cb924b734e
Daniel Chabr <danielchabr@gmail.com>
2017-01-08 21:44:08 +0100

    init

---
+++ test.py
@@ -0,0 +1,66 @@
+import cv2
+import os, urllib
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import mxnet as mx
+from collections import namedtuple
+Batch = namedtuple('Batch', ['data'])
+
+IMAGE_SIZE = 224
+MODEL = 'model/resnet'
+
+def get_image(url, show=True):
+    filename = 'test/' +  url.split("/")[-1]
+    urllib.urlretrieve(url, filename)
+    img = cv2.imread(filename)
+    if img is None:
+        print('failed to download ' + url)
+    return filename
+
+def predict(filename, mod, classes):
+    img = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
+    if img is None:
+        return None
+    img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+    img = np.swapaxes(img, 0, 2)
+    img = np.swapaxes(img, 1, 2) 
+    img = img[np.newaxis, :] 
+    
+    mod.forward(Batch([mx.nd.array(img)]))
+    prob = mod.get_outputs()[0].asnumpy()
+    print(prob)
+    prob = np.squeeze(prob)
+
+    a = np.argsort(prob)[::-1]    
+    for i in a[0:5]:
+        print('probability=%f, class=%s' %(prob[i], classes[i]))
+    return classes[a[0:5][0]]
+
+if __name__ == '__main__':
+    sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL, 5)
+    mod = mx.mod.Module(symbol=sym)
+    mod.bind(for_training=False, data_shapes=[('data', (1,3,IMAGE_SIZE,IMAGE_SIZE))])
+    mod.set_params(arg_params, aux_params)
+
+    TEST_PATH = 'test/categories/'
+    total = .0
+    correct = .0
+    classes = [f for f in listdir(TEST_PATH) if os.path.isdir(join(TEST_PATH, f))]
+    for c in classes:
+        print(c)
+        files = [f for f in listdir(TEST_PATH + c) if isfile(join(TEST_PATH + c, f))]
+        for f in files:
+            filepath = TEST_PATH + c + '/' + f
+            result = predict(filepath, mod, classes)
+            total += 1.0
+            if result == c:
+                print('CORRECT')
+                correct += 1.0
+            else:
+                print('FALSE')
+
+    print('Validation accuracy: ' + str(correct/total))
+
+
+
---
+++ train.py
@@ -0,0 +1,97 @@
+import os
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from common import find_mxnet, data, fit
+from common.util import download_file
+import mxnet as mx
+import requests
+import json
+import codecs
+import sys, time
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+LABELS = []#['beagle dog', 'husky dog', 'dalmatian dog']
+API_KEY = '4196410-648a310def2eb58655fe4fa70'
+MXNET_HOME = './..'
+DATA_DIR = 'data/categories'
+IMAGE_SIZE = '224'
+NETWORK = 'resnet'
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def downloadImages(label, links):
+    for link in links:
+        name = os.path.join(DATA_DIR, label, link.split('/')[-1])
+        download_file(link, name)
+
+def getImagesForLabel(label):
+    URL = "https://pixabay.com/api/?key="+API_KEY+"&per_page=200&q="+label
+    resp = requests.post(URL, verify=False)
+    if resp.status_code != 200:
+        print "Failed to get images for label " + label
+        print resp.text
+        sys.exit(-1)
+    respJson = resp.json()
+    imagesJson = respJson['hits']
+    imageLinks = []
+    for imageJson in imagesJson:
+        imageLinks.append(imageJson['webformatURL'])
+    return imageLinks
+
+if __name__ == '__main__':
+
+    for label in LABELS:
+        imageLinks = getImagesForLabel(label)
+        downloadImages(label, imageLinks)
+
+    # TRAIN DATASET
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --list=1 --recursive=1 --shuffle=1 --train-ratio=0.8 data/list ' + DATA_DIR)
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --num-thread=4 --resize=' + IMAGE_SIZE + ' --quality 90 --color=1 data/list ' + DATA_DIR)
+
+    # TEST DATASET
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --list=1 --recursive=1 --shuffle=1  test/list test/categories')
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --num-thread=4 --resize=' + IMAGE_SIZE + ' --color=1 test/list test/categories')
+
+    # parse args
+    parser = argparse.ArgumentParser(description="train dogs",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    fit.add_fit_args(parser)
+    data.add_data_args(parser)
+    data.add_data_aug_args(parser)
+    data.set_data_aug_level(parser, 3)
+    parser.set_defaults(
+        #gpus           = '0',
+        # network
+        network        = NETWORK,
+        num_layers     = 101,
+        # data
+        data_train     = 'data/list_train.rec',
+        data_val       = 'data/list_val.rec',
+        num_classes    = 3,
+        num_examples   = 150,
+        image_shape    = '3,' + IMAGE_SIZE + ',' + IMAGE_SIZE,
+        pad_size       = 4,
+        min_random_scale = 1, # if input image has min size k, suggest to use
+        top_k          = 1,
+
+        # train
+        batch_size     = 30,
+        disp_batches   = 1,
+        num_epochs     = 10,
+        lr             = .01,
+        lr_factor      = 0.8,
+        lr_factor_epoch = 0.5,
+        lr_step_epochs = '100',
+        #save model
+        model_prefix   =  'model/' + NETWORK
+    )
+    args = parser.parse_args()
+
+    # load network
+    from importlib import import_module
+    net = import_module('symbol.'+args.network)
+    sym = net.get_symbol(**vars(args))
+    
+    # train
+    fit.fit(args, sym, data.get_rec_iter)
