a139380891f2f0b6c7f89ac4fafd18ed312dad2d
Daniel Chabr <danielchabr@gmail.com>
2017-01-09 10:13:18 +0100

    finetune and test

---
+++ finetune.py
@@ -0,0 +1,116 @@
+import os
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from common import find_mxnet, data, fit
+from common.util import download_file
+import mxnet as mx
+import requests
+import json
+import codecs
+import sys, time
+import logging
+head = '%(asctime)-15s %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=head)
+
+MXNET_HOME = './..'
+DATA_DIR = 'data/categories'
+IMAGE_SIZE = 120
+IMAGE_SIZE_STR = str(IMAGE_SIZE)
+NETWORK = 'resnet'
+
+def get_iterators(batch_size, data_shape=(3, IMAGE_SIZE, IMAGE_SIZE)):
+    train = mx.io.ImageRecordIter(
+        path_imgrec         = './data/list_train.rec',
+        data_name           = 'data',
+        label_name          = 'softmax_label',
+        batch_size          = batch_size,
+        data_shape          = data_shape,
+        shuffle             = True,
+        rand_crop           = True,
+        rand_mirror         = True)
+    val = mx.io.ImageRecordIter(
+        path_imgrec         = './data/list_val.rec',
+        data_name           = 'data',
+        label_name          = 'softmax_label',
+        batch_size          = batch_size,
+        data_shape          = data_shape,
+        rand_crop           = False,
+        rand_mirror         = False)
+    return (train, val)
+
+def get_model(prefix, epoch):
+    fname1 = prefix+'-symbol.json'
+    fname2 = prefix+'-%04d.params' % (epoch,)
+    if not os.path.isfile(fname1.split('/')[-1]):
+        download_file(prefix+'-symbol.json')
+    if not os.path.isfile(fname2.split('/')[-1]):
+        download_file(prefix+'-%04d.params' % (epoch,))
+
+def get_fine_tune_model(symbol, arg_params, num_classes, layer_name='flatten0'):
+    """
+    symbol: the pre-trained network symbol
+    arg_params: the argument parameters of the pre-trained model
+    num_classes: the number of classes for the fine-tune datasets
+    layer_name: the layer name before the last fully-connected layer
+    """
+    all_layers = sym.get_internals()
+    net = all_layers[layer_name+'_output']
+    net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
+    net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
+    new_args = dict({k:arg_params[k] for k in arg_params if 'fc1' not in k})
+    return (net, new_args)
+
+def _save_model():
+    model_prefix = 'model/resnet'
+    dst_dir = os.path.dirname(model_prefix)
+    if not os.path.isdir(dst_dir):
+        os.mkdir(dst_dir)
+    return mx.callback.do_checkpoint(model_prefix)
+
+def fit(symbol, arg_params, aux_params, train, val, batch_size, num_gpus):
+    devs = mx.cpu() if num_gpus is None or args.gpus is 0 else [
+        mx.gpu(i) for i in range(num_gpus)]
+    #devs = [mx.gpu(i) for i in range(num_gpus)]
+
+    model_prefix   =  'model/' + NETWORK
+    # save model
+    checkpoint = _save_model()
+
+    mod = mx.mod.Module(symbol=new_sym, context=devs)
+    mod.bind(data_shapes=train.provide_data, label_shapes=train.provide_label)
+    mod.init_params(initializer=mx.init.Xavier(rnd_type='gaussian', factor_type="in", magnitude=2))
+    mod.set_params(new_args, aux_params, allow_missing=True)
+    mod.fit(train, val, 
+        num_epoch=8,
+        batch_end_callback = mx.callback.Speedometer(batch_size, 1),        
+        epoch_end_callback = checkpoint,
+        kvstore='device',
+        optimizer='sgd',
+        optimizer_params={'learning_rate':0.005},
+        eval_metric='acc')
+    return mod.score(val, 'acc')
+
+if __name__ == '__main__':
+
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --list=1 --recursive=1 --shuffle=1 --train-ratio=0.9 data/list ' + DATA_DIR)
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --num-thread=16 --resize=' + IMAGE_SIZE_STR + ' data/list ' + DATA_DIR)
+
+    get_model('http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50', 0)
+    sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-50', 0)
+
+    # Output may vary
+    num_classes = 3
+    #batch_per_gpu = 16
+    #num_gpus = 8
+
+    (new_sym, new_args) = get_fine_tune_model(sym, arg_params, num_classes)
+
+    #Additional for local only
+    #batch_size = batch_per_gpu * num_gpus
+    num_gpus = None
+    batch_size = 15
+
+    (train, val) = get_iterators(batch_size)
+    mod_score = fit(new_sym, new_args, aux_params, train, val, batch_size, num_gpus)
+    assert mod_score > 0.77, "Low training accuracy."
--- test.py
+++ test.py
@@ -7,8 +7,11 @@ import mxnet as mx
 from collections import namedtuple
 Batch = namedtuple('Batch', ['data'])
 
-IMAGE_SIZE = 224
+IMAGE_SIZE = 100
 MODEL = 'model/resnet'
+MXNET_HOME = './..'
+DATA_DIR = 'data/categories'
+IMAGE_SIZE_STR = str(IMAGE_SIZE)
 
 def get_image(url, show=True):
     filename = 'test/' +  url.split("/")[-1]
@@ -29,36 +32,40 @@ def predict(filename, mod, classes):
     
     mod.forward(Batch([mx.nd.array(img)]))
     prob = mod.get_outputs()[0].asnumpy()
-    print(prob)
+    #print(prob)
     prob = np.squeeze(prob)
 
     a = np.argsort(prob)[::-1]    
-    for i in a[0:5]:
-        print('probability=%f, class=%s' %(prob[i], classes[i]))
+    #for i in a[0:5]:
+        #print('probability=%f, class=%s' %(prob[i], classes[i]))
     return classes[a[0:5][0]]
 
 if __name__ == '__main__':
-    sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL, 5)
+    sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL, 1)
     mod = mx.mod.Module(symbol=sym)
     mod.bind(for_training=False, data_shapes=[('data', (1,3,IMAGE_SIZE,IMAGE_SIZE))])
     mod.set_params(arg_params, aux_params)
 
     TEST_PATH = 'test/categories/'
+
+    # TEST DATASET
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --list=1 --recursive=1 --shuffle=1  test/list test/categories')
+    os.system('python ' + MXNET_HOME + '/tools/im2rec.py --num-thread=4 --resize=' + IMAGE_SIZE_STR + ' --color=1 test/list test/categories')
+
     total = .0
     correct = .0
     classes = [f for f in listdir(TEST_PATH) if os.path.isdir(join(TEST_PATH, f))]
     for c in classes:
-        print(c)
         files = [f for f in listdir(TEST_PATH + c) if isfile(join(TEST_PATH + c, f))]
         for f in files:
             filepath = TEST_PATH + c + '/' + f
             result = predict(filepath, mod, classes)
             total += 1.0
             if result == c:
-                print('CORRECT')
+                #print('CORRECT ' + filepath)
                 correct += 1.0
             else:
-                print('FALSE')
+                print('FALSE ' + filepath)
 
     print('Validation accuracy: ' + str(correct/total))
 
